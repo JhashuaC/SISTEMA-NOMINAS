a83b4f9cdd25411af1d062a7068cc836de2f4b3b
Marco Ugalde <marco.u.c0312@gmail.com>
2025-09-04 15:34:57 -0600

    Update BonificableInterface.java

--- src/main/java/modelo/BonificableInterface.java
+++ src/main/java/modelo/BonificableInterface.java
@@ -1,6 +1,15 @@
 package modelo;
 
+/**
+ * Interfaz que define la bonificación de un empleado. Permite calcular la
+ * bonificación que corresponde agregar al salario.
+ */
 public interface BonificableInterface {
 
+    /**
+     * Calcula la bonificación del empleado.
+     *
+     * @return Monto de la bonificación.
+     */
     public double Bono();
 }
